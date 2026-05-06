01961bf5f995c40e528f8c0b36315675039b8278
Ahmadmunim21 <bymunimyaziz@gmail.com>
2026-05-06 11:53:54 +0800

    add list and tuples

---
+++ py/ch06.py
@@ -0,0 +1,27 @@
+fruits = ["apple", "banana", "orange"]
+#numbers = [1, 2, 3, 4, 5]
+#mixed = ["hello", 42, 3.14, True]
+#empty_list = []
+
+# Accesing Elements
+# print(fruits[0])    #"apple"
+# print(fruits[-1])   #"orange"
+# print(fruits[1:3])  #['banana', 'orange']
+# print(fruits[:2])   #['apple', 'banana']
+# print(fruits[2:])   #['orange']
+
+
+# fruits.append("durian")         #Add to end ["apple", "banana", "orange", "durian"]
+# #fruits.insert(2, "ciku")       #Insert at index 2 ["apple", "banana", "ciku", "orange", "durian]
+# #fruits.remove("orange")        #Remove by value ["apple", "orange", "durian]  
+# popped = fruits.pop()           #Remove and return last
+# fruits.sort()
+# fruits.reverse()
+
+# List Operations
+#len (fruits)                        # Length
+"apple" in fruits                   # Check membership
+fruits + ["mango"]                  # Concatenation
+fruits * 2                          # Repetition
+
+print(fruits)
---
+++ py/ch07.py
@@ -0,0 +1,8 @@
+coordinates = (1.1231231,5.6721275)
+person = ("Alice", 25 , "Engineer")
+single_item = (42,) #Comma for single item
+
+#Tuple operations
+print(coordinates[1])   
+print(len(person))
+
